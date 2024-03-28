6fc3d279da832d7f109ced40d108679aba0f9aa2
mohanty-ritesh <mohantyritesh811@gmail.com>
2024-03-28 06:52:25 +0500

    mail sec changes

--- app/Components/EmailSection.jsx
+++ app/Components/EmailSection.jsx
@@ -10,7 +10,9 @@ const EmailSection = () => {
     initial: { opacity: 0, y: 50 },
     animate: { opacity: 1, y: 0 },
   };
-
+  const onSubmit=async ()=>{
+    
+  }
   return (
     <section className="ContactMe bg-gradient-to-r from-[#c5c0c1] to-[#8e779a] flex justify-center items-center w-screen h-screen">
       <motion.form
@@ -27,7 +29,7 @@ const EmailSection = () => {
         <input className="bg-[#A9A7B5] border-2 border-black rounded-lg outline-none text-[#2c2c2e] mb-4 py-4 px-3 placeholder-gray-600 w-full" type="email" id="email" placeholder="Your email" required />
         <input className="bg-[#A9A7B5] border-2 border-black rounded-lg outline-none text-[#2c2c2e] mb-4 py-4 px-3 placeholder-gray-600 w-full" type="text" id="phone" placeholder="Your Phone no." required />
         <textarea className="bg-[#A9A7B5] border-2 border-black rounded-lg outline-none text-[#2c2c2e] mb-4 py-3 px-3 h-60 resize-none placeholder-gray-600 w-full" id="message" placeholder="Your Message..." ></textarea>
-        <input className="border-2 border-[#630817] font-bold rounded-3xl bg-[#f25454] text-[#630817] px-8 py-2 my-5 hover:cursor-pointer" type="button" value="Button" />
+        <input className="border-2 border-[#630817] font-bold rounded-3xl bg-[#f25454] text-[#630817] px-8 py-2 my-5 hover:cursor-pointer" type="button" value="Button" onClick={onSubmit} />
       </motion.form>
     </section>
   );
