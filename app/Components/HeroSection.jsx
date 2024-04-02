eed8ad19e8214b6fba7fbe889dd29f2bc43c6f72
mohanty-ritesh <mohantyritesh811@gmail.com>
2024-04-02 20:39:09 +0500

    hsec

--- app/Components/HeroSection.jsx
+++ app/Components/HeroSection.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 function HeroSection() {
   return (
-    <section className="h-[86vh] flex justify-center ">
+    <section className="h-[86vh] flex justify-center pt-20 lg:pt-2 ">
       <div className="grid grid-cols-1 sm:grid-cols-12 ">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
@@ -14,7 +14,7 @@ function HeroSection() {
           transition={{ duration: 0.3 }}
           className="col-span-7 place-self-center text-center pl-7  sm:text-left"
         >
-          <h1 className="text-white mb-4 text-5xl sm:text-6xl lg:text-8xl xl:text-8xl font-extrabold">
+          <h1 className="text-white mb-4  text-4xl sm:text-4xl p-auto lg:text-8xl xl:text-8xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#fddd58]  to-[#fd914d]">
               Hello, I'm{" "}
             </span>
@@ -35,7 +35,7 @@ function HeroSection() {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] py-5 text-lg sm:text-lg lg:text-xl xl:text-xl mb-6">
+          <p className="text-[#ADB7BE] py-5 text-base sm:text-lg lg:text-xl xl:text-xl mb-6">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio
             sint enim, velit ipsum libero est! Aliquid unde aperiam voluptatem
             officia!
@@ -47,12 +47,12 @@ function HeroSection() {
             Download my cv
           </button>
         </motion.div>
-       <div className="col-span-5 place-self-center pl-6 mt-4 ml-3 lg:mt-0 overflow-hidden">
+       <div className="col-span-5 place-self-center pb-4 pl-6 mt-auto  ml-3 lg:mt-0 overflow-hidden">
   <motion.div
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     transition={{ duration: 0.2 }}
-    className="rounded-full  sm:block   w-[300px] h-[300px] md:w-[200px] md:h-[200px] lg:w-[350px] lg:h-[400px] relative "
+    className="rounded-full  sm:block   w-[230px] h-[230px]   lg:w-[350px] lg:h-[400px] relative "
   >
     <Image
       src="/Images/reps.png"
